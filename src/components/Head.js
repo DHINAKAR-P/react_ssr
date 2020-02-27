ea3325c5587f408ad251240cffd828d2f050188e
DHINAKAR-P <DHINAKAR-P>
2020-02-27 16:56:04 +0530

    Head file bug fix

--- src/components/Head.js
+++ src/components/Head.js
@@ -17,15 +17,6 @@ const Head = () => {
         content="Boilerplate for React apps with routing & server side rendering"
       />
       <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
-
-      <link
         rel="shortcut icon"
         href={`${process.env.PUBLIC_URL}/favicon.ico?v1`}
       />
